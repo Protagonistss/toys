c18d910645cceaaaa76cfff6287193371ab4690c
protagonisths <13837050616@163.com>
2022-04-14 19:41:11 +0800

    feat(esbuild): gen html

---
+++ packages/esbuild/home.jsx
@@ -0,0 +1,6 @@
+// import React from 'react'
+import { render } from 'https://cdn.skypack.dev/react-dom'
+
+const RootElement = () => <h1>hello home</h1>
+
+render(<RootElement></RootElement>, document.getElementById('root'))
--- packages/esbuild/scripts/build.js
+++ packages/esbuild/scripts/build.js
@@ -1,17 +1,18 @@
 const { build } = require('esbuild')
 const CDN = require('../plugins/import-cdn')
+const HTML = require('../plugins/gen-html')
 
 const entry = async () => {
   build({
     absWorkingDir: process.cwd(),
-    entryPoints: ['./main.jsx'],
+    entryPoints: ['./main.jsx', './home.jsx'],
     outdir: 'dist',
     bundle: true,
     format: 'esm',
     splitting: true,
     sourcemap: true,
     metafile: true,
-    plugins: [CDN()]
+    plugins: [HTML(), CDN()]
   }).then(() => {
     console.log('build finished')
   })
